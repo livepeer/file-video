453d4645f1ee0844e2ba31b1825bd4f407d3e813
Julián Benegas <julianbenegas99@gmail.com>
2020-08-18 17:32:59 -0300

    trigger deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import { ThemeProvider } from "theme-ui";
 import theme from "lib/theme";
 import "css/global.css";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
-
-export default MyApp;
