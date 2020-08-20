99914767ffc09a04cbb969619370b60b210e5169
Julián Benegas <julianbenegas99@gmail.com>
2020-08-20 14:24:36 -0300

    add canvas width and height

--- components/particles-background/index.tsx
+++ components/particles-background/index.tsx
@@ -5,21 +5,27 @@ import canvasEffect from "./effect";
 
 const ParticlesBackground = () => {
   const [isInitialized, setIsInitialized] = useState(false);
+  const [viewportSize, setViewportSize] = useState<{
+    width: number;
+    height: number;
+  }>();
 
   useEffect(() => {
     if (isInitialized) return;
+    if (!viewportSize) {
+      setViewportSize({ width: window.innerWidth, height: window.innerHeight });
+      return;
+    }
     setIsInitialized(true);
     canvasEffect();
-  }, [isInitialized]);
+  }, [isInitialized, viewportSize]);
 
   return (
     <canvas
       id="particles-canvas"
-      sx={{
-        width: "100vw",
-        height: "100vh",
-        position: "absolute",
-      }}
+      width={viewportSize?.width}
+      height={viewportSize?.height}
+      sx={{ position: "absolute" }}
     />
   );
 };
