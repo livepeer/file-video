e500d888689e8f079e93b80558f5690da79f9ea3
Adam Soffer <adam@livepeer.org>
2020-10-21 19:04:43 -0400

    add analytics (#12)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,21 @@ import { ThemeProvider } from "theme-ui";
 import theme from "lib/theme";
 import "css/global.css";
 import 'video.js/dist/video-js.css';
+import * as gtag from "../lib/gtag"
+import { useEffect } from "react"
+import Router from "next/router"
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    Router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [])
+  
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
