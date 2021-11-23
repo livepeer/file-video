ca43c2241386998dec15243e5bee5289ac52baf1
adamsoffer <adam@livepeer.org>
2021-11-23 00:51:25 -0500

    throw 404 on no longer supported page

--- pages/v/[id].tsx
+++ pages/v/[id].tsx
@@ -7,97 +7,99 @@ import Spinner from "../../components/spinner";
 import { useRouter } from "next/router";
 import ViewportHeightBox from "components/viewport-height-box";
 import { useEffect, useState } from "react";
+import DefaultErrorPage from "next/error";
 
-export async function getStaticProps({ params: { id: id } }) {
-  const assetURL = process.env.DEMUX_URL + 'asset';
+export default () => <DefaultErrorPage statusCode={404} />;
+// export async function getStaticProps({ params: { id: id } }) {
+//   const assetURL = process.env.DEMUX_URL + "asset";
 
-  const res = await fetch(assetURL + '/' + id);
-  const asset = await res.json();
+//   const res = await fetch(assetURL + "/" + id);
+//   const asset = await res.json();
 
-  const src = asset['stream_url'];
-  const poster = asset['thumbnail'];
+//   const src = asset["stream_url"];
+//   const poster = asset["thumbnail"];
 
-  return { props: { src, poster } };
-}
+//   return { props: { src, poster } };
+// }
 
-export function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: true,
-  };
-}
+// export function getStaticPaths() {
+//   return {
+//     paths: [],
+//     fallback: true,
+//   };
+// }
 
-export default function Playback({ src, poster }) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
+// export default function Playback({ src, poster }) {
+//   const router = useRouter();
+//   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (router.isFallback) return;
-    fetch(src).then((res) => {
-      if (res.status === 404) router.push("/");
-      else setIsLoading(false);
-    });
-  }, [router]);
+//   useEffect(() => {
+//     if (router.isFallback) return;
+//     fetch(src).then((res) => {
+//       if (res.status === 404) router.push("/");
+//       else setIsLoading(false);
+//     });
+//   }, [router]);
 
-  if (isLoading) {
-    return (
-      <Layout>
-        <ViewportHeightBox
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Spinner />
-        </ViewportHeightBox>
-      </Layout>
-    );
-  }
+//   if (isLoading) {
+//     return (
+//       <Layout>
+//         <ViewportHeightBox
+//           sx={{
+//             display: "flex",
+//             alignItems: "center",
+//             justifyContent: "center",
+//           }}
+//         >
+//           <Spinner />
+//         </ViewportHeightBox>
+//       </Layout>
+//     );
+//   }
 
-  return (
-    <Layout>
-      <ViewportHeightBox
-        sx={{
-          display: ["flex", null, null, null, "grid"],
-          flexDirection: "column",
-          justifyContent: "center",
-          gridTemplateColumns: ["1fr", null, null, null, "1fr 1fr"],
-          alignItems: "center",
-        }}
-      >
-        <div>
-          <Heading
-            variant="heading.1"
-            sx={{ textAlign: ["center", null, null, null, "left"] }}
-          >
-            This video is ready for playback
-          </Heading>
-          <Text
-            variant="normal"
-            sx={{
-              mb: [4, null, null, null, 5],
-              mt: [3, 4],
-              mx: ["auto", null, null, null, 0],
-              maxWidth: "587px",
-              textAlign: ["center", null, null, null, "left"],
-            }}
-          >
-            <span sx={{ display: ["none", "inline"] }}>
-              file.video is the easiest way to upload, edit, and share video.
-              It’s free to use and there’s no signup required.
-              <br />
-              <br />
-            </span>
-            Go{" "}
-            <Link href="/" passHref>
-              <A variant="accent">back home</A>
-            </Link>{" "}
-            to upload another video.
-          </Text>
-        </div>
-        <VideoPlayer src={src} poster={poster} />
-      </ViewportHeightBox>
-    </Layout>
-  );
-}
+//   return (
+//     <Layout>
+//       <ViewportHeightBox
+//         sx={{
+//           display: ["flex", null, null, null, "grid"],
+//           flexDirection: "column",
+//           justifyContent: "center",
+//           gridTemplateColumns: ["1fr", null, null, null, "1fr 1fr"],
+//           alignItems: "center",
+//         }}
+//       >
+//         <div>
+//           <Heading
+//             variant="heading.1"
+//             sx={{ textAlign: ["center", null, null, null, "left"] }}
+//           >
+//             This video is ready for playback
+//           </Heading>
+//           <Text
+//             variant="normal"
+//             sx={{
+//               mb: [4, null, null, null, 5],
+//               mt: [3, 4],
+//               mx: ["auto", null, null, null, 0],
+//               maxWidth: "587px",
+//               textAlign: ["center", null, null, null, "left"],
+//             }}
+//           >
+//             <span sx={{ display: ["none", "inline"] }}>
+//               file.video is the easiest way to upload, edit, and share video.
+//               It’s free to use and there’s no signup required.
+//               <br />
+//               <br />
+//             </span>
+//             Go{" "}
+//             <Link href="/" passHref>
+//               <A variant="accent">back home</A>
+//             </Link>{" "}
+//             to upload another video.
+//           </Text>
+//         </div>
+//         <VideoPlayer src={src} poster={poster} />
+//       </ViewportHeightBox>
+//     </Layout>
+//   );
+// }
